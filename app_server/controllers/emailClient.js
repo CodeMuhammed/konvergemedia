cd6be05dcf2f501b45a223c16e2ad7441c3f93da
codemuhammed <codemuhammed@gmail.com>
2016-05-16 21:25:10 +0100

    Signed-off-by: codemuhammed <codemuhammed@gmail.com>

--- app_server/controllers/emailClient.js
+++ app_server/controllers/emailClient.js
@@ -1,3 +1,52 @@
+//
+/*var nodemailer =  require('nodemailer');
+
+// create reusable transporter object using SMTP transport
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        XOAuth2 :{
+           user: "your_email_address@gmail.com",
+           clientId: "your_client_id",
+           clientSecret: "your_client_secret",
+           refreshToken: "your_refresh_token"
+        }
+    }
+});
+
+//
+module.exports = function(){
+
+  function sendEmail(htmldata , email , subject  , attachment, cb){
+      // setup e-mail data with unicode symbols
+      var mailOptions = {
+          from: 'digifyBytes Africa <certificate@digifyafrica.com>', // sender address
+          to: email, // list of receivers
+          //cc:'second@domain.com',
+   		   //bcc:'secretagent@company.gov',
+   		   subject: subject,
+   		   'h:Reply-To': 'certificate@digifyafrica.com',
+   		   html: htmlData,
+         attachments:[attachment]
+      };
+
+      // send mail with defined transport object
+      transporter.sendMail(mailOptions, function(error, info){
+          if(error){
+              cb(error , null);
+          }
+          else {
+              cb(null , info);
+          }
+
+      });
+  }
+
+	return {
+		sendEmail : sendEmail
+	}
+}*/
+
 var nodemailer =  require('nodemailer');
 var mg = require('nodemailer-mailgun-transport');
 
@@ -13,19 +62,19 @@ var auth = {
 var nodemailerMailgun = nodemailer.createTransport(mg(auth));
 
 module.exports = function(){
-	
+
     function sendEmail(htmlData , email , subject , attachment ,  cb){
 		var options = {
-		   from: 'palingramblog@gmail.com',
+		   from: 'digifyBytes Africa <certificate@digifyafrica.com>',
 		   to: email, // An array if you have multiple recipients.
 		   //cc:'second@domain.com',
 		   //bcc:'secretagent@company.gov',
 		   subject: subject,
-		   'h:Reply-To': 'codemuhammed@gmail.com',
+		   'h:Reply-To': 'certificate@digifyafrica.com',
 		   html: htmlData,
 		   attachment: attachment
 		};
-		
+
 		nodemailerMailgun.sendMail(options, function (err, info) {
 			if (err) {
 				console.log(err);
@@ -41,4 +90,4 @@ module.exports = function(){
 	return {
 		sendEmail : sendEmail
 	};
-} 
+}
