123d4b79ed4a0de78932d99168773a12c01e5a64
codemuhammed <codemuhammed@gmail.com>
2016-06-26 21:31:08 +0100

    Signed-off-by: codemuhammed <codemuhammed@gmail.com>

--- public/js/custom/app.js
+++ public/js/custom/app.js
@@ -524,7 +524,7 @@ angular.module('digifyBytes' , ['ui.router' ,'mgcrea.ngStrap' , 'mgcrea.ngStrap.
               success: function(files) {
                   $timeout(function(){
                       console.log(files[0].link);
-                      $scope.certTemplate.imgUrl = files[0].link.substr(0 , files[0].link.length-1)+'1';
+                      $scope.certTemplate.imgUrl = files[0].link.substr(0 , files[0].link.length-1)+'1&raw=1';
                       console.log($scope.certTemplate.imgUrl);
 
                       //inferr category name from file name
