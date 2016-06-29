3f2cea1e9771e8cb2e8127a21e1986ded198f98a
codemuhammed <codemuhammed@gmail.com>
2016-06-29 23:46:49 +0100

    Signed-off-by: codemuhammed <codemuhammed@gmail.com>

--- public/js/custom/app.js
+++ public/js/custom/app.js
@@ -34,30 +34,112 @@ angular.module('digifyBytes' , ['ui.router' ,'mgcrea.ngStrap' , 'mgcrea.ngStrap.
     }
 ])
 
-//custom directive to detect when an image is done loading
-.directive('imageloadnotifier', function($timeout) {
+//custom directive monitor image loading process from start to finish
+.directive('awesomeImg', function($timeout) {
        return {
-           restrict: 'A',
-           scope:false,
-           link: function(scope, element, attrs) {
-               console.log(element);
-               element.bind('load', function() {
+           restrict: 'E',
+           scope:{
+              url : '@url',
+              style  : '@style',
+              boxX   : '@boxx',
+              boxY   : '@boxy',
+           },
+           template: [
+               '<div class="spinner-c" ng-show="imageLoading.statusType != \'loaded\'">',
+                   '<div class="overlay" style="width:{{boxX}}px; height:{{boxY}}px;">',
+                     '<div class="spinner">',
+                         '<div class="double-bounce1"></div>',
+                         '<div class="double-bounce2"></div>',
+                     '</div>',
+                     '<div class="msg">',
+                         '<div ng-switch on="imageLoading.statusType">',
+                             '<div ng-switch-when="error">',
+                                  'Template was not loaded. Click to',
+                                  '<button  class="btn btn-xs btn-default" ng-click="reloadImg()">',
+                                    'retry </button> ...',
+                             '</div>',
+                             '<div ng-switch-when="retry">',
+                                  'Attempting to reload image please wait...',
+                             '</div>',
+                             '<div ng-switch-when="loading">',
+                                  'Template loading {{imageLoading.progress}}% ..',
+                             '</div>',
+                             '<div ng-switch-when="loaded">',
+                                  'Template loading complete {{imageLoading.progress}}%',
+                             '</div>',
+                         '</div>',
+                     '</div>',
+                   '</div>',
+                '</div>',
+                '<img ng-src="{{imageLoading.url}}" alt="" style="{{style}}" />',
+         ].join(''),
+         controller: function($scope , $timeout){
+              //Define the image loading object that binds to the view
+              $scope.imageLoading = {
+                   statusType : 'loading',
+                   url : '',
+                   progress: 0
+              };
+
+              $scope.$watch('url' , function(newVal , oldVal){
+                    if(oldVal != newVal && angular.isDefined(newVal)){
+                        loadImage(newVal);
+                    }
+              });
+
+              //
+              function loadImage(url){
+                  $scope.imageLoading.url = 'phooney.png';
+                  var imageUrl = url;
+                  //DO the xhr dance
+                  var imgObj = new Image();
+                  var request = new XMLHttpRequest();
+                  request.onprogress = function(event) {
+                    $timeout(function(){
+                        if (!event.lengthComputable) {
+                          return;
+                        }
+                        var loaded = event.loaded;
+                        var total = event.total;
+                        $scope.imageLoading.progress = (loaded / total).toFixed(2)*100;
+                    });
+                  };
+
+                  request.onload = function(event) {
+                    console.log('loading', imageUrl);
+                    $scope.imageLoading.statusType = 'loading';
+                    request.onloadend = function(event){
+                        $timeout(function(){
+                            $scope.imageLoading.statusType = 'loaded';
+                            $scope.imageLoading.url = imageUrl;
+                            console.log('Complete' , imageUrl);
+                        } , 100);
+                    };
+                  };
+
+                  request.onerror = function(event) {
+                    $timeout(function(){
+                        $scope.imageLoading.statusType = 'error';
+                        console.log('error');
+                    });
+
+                  }
+
+                  //Retrieve the image from the server
+                  request.open('GET', imageUrl, true);
+                  request.overrideMimeType('text/plain; charset=x-user-defined');
+                  request.send(null);
+              }
+
+              //Releads the image
+              $scope.reloadImg = function(){
+                  $scope.imageLoading.statusType = 'retry';
                   $timeout(function(){
-                      scope.imageLoadStatus({type:'load'});
-                  });
-               });
-               element.bind('progress', function() {
-                  $timeout(function(){
-                      alert('here');
-                  });
-               });
-               element.bind('error', function(){
-                   $timeout(function(){
-                       scope.imageLoadStatus({type:'error'});
-                   });
-               });
-           }
-       };
+                        loadImage($scope.url);
+                  } , 2000);
+              }
+         }
+    };
 })
 
 //A directive for reading txt files
@@ -545,9 +627,9 @@ angular.module('digifyBytes' , ['ui.router' ,'mgcrea.ngStrap' , 'mgcrea.ngStrap.
 //
 .controller('dragController'  , function($scope  , $state , $timeout, $document , Roles , Auth){
      //Bounce users who are not yet auth to home
-     if(!Auth.isAuth()){
+     /*if(!Auth.isAuth()){
          $state.go('home');
-     }
+     }*/
 
      //Disble scrolling to pin dispaly down
      $document.find('html').css({overflow:'hidden'});
@@ -560,27 +642,6 @@ angular.module('digifyBytes' , ['ui.router' ,'mgcrea.ngStrap' , 'mgcrea.ngStrap.
          $scope.view = view;
      }
 
-     //Listens for when the image loading status changes..
-     $scope.imageLoading = {
-         msg:'',
-         status:false,
-         statusType:''
-     };
-     $scope.imageLoadStatus = function(stats){
-          $scope.imageLoading.statusType = stats.type;
-          switch (stats.type) {
-            case 'load':{
-               $scope.imageLoading.status = false;
-               break;
-            };
-
-            case 'error':{
-               console.log(stats);
-               $scope.imageLoading.status = true;
-               break;
-            };
-          }
-     }
 
      //
      Roles.certsAsync().then(
@@ -708,10 +769,8 @@ angular.module('digifyBytes' , ['ui.router' ,'mgcrea.ngStrap' , 'mgcrea.ngStrap.
     $scope.changeActiveTemplate = function(template){
         if(template){
           $scope.certTemplate = template;
-          console.log($scope.certTemplate);
         }
         else{
-           $scope.imageLoading.status = false;
            //Temporarily load a different template from this outline
            var temp = $scope.certTemplate;
            $scope.certTemplate = '';
