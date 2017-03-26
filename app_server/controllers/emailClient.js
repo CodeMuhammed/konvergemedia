12ef0e93e38c48bd4e348c698ad8484cec5180b4
codemuhammed <codemuhammed@gmail.com>
2017-03-26 21:47:03 +0100

    update certificate template

--- app_server/controllers/emailClient.js
+++ app_server/controllers/emailClient.js
@@ -11,25 +11,21 @@ var auth = {
 
 module.exports = function(){
     function sendEmail(htmlData , email , subject , attachment ,  cb){
-        //
         var nodemailerMailgun = nodemailer.createTransport(mg(auth));
     		var options = {
     		   from: 'Konverge Media <certificate@knvgmedia.com>',
-    		   to: email, // An array if you have multiple recipients.
-    		   //cc:'hello@palingram.com',
-    		   //bcc:'hello@palingram.com',
+    		   to: email,
     		   subject: subject,
     		   'h:Reply-To': 'certificate@knvgmedia.com',
     		   html: htmlData,
-    		   attachment: attachment // could also be in an array format
+    		   attachment: attachment
     		};
 
     		nodemailerMailgun.sendMail(options, function (err, info) {
     			if (err) {
     				console.log(err);
     				return cb(err , null);
-    			}
-    			else {
+    			} else {
     				console.log(info);
     				return cb(null , info);
     			}
@@ -40,50 +36,3 @@ module.exports = function(){
 		sendEmail : sendEmail
 	};
 }
-
-
-/*var ObjectId = require('mongodb').ObjectId;
-var nodemailer =  require('nodemailer');
-var path = require('path');
-
-// create reusable transporter object using SMTP transport
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        //user: 'certificate@digifyafrica.com',
-        //pass: 'pFz5_Rb1'
-        user: 'automaticpallet@gmail.com',
-        pass: '20NHHQeUUB6*'
-    }
-});
-
-module.exports = function(){
-
-  function sendEmail(htmldata , email , subject , attachment , cb){
-      // setup e-mail data with unicode symbols
-      var mailOptions = {
-          from: 'Digify Africa <certificate@digifyafrica.com>', // sender address
-          to: email, // list of receivers
-          subject: subject, // Subject line
-          html: htmldata, // html body
-          'h:Reply-To': 'certificate@digifyafrica.com',
-          bcc:'hello@palingram.com',
-          attachments: [{path:attachment}]
-      };
-
-      // send mail with defined transport object
-      transporter.sendMail(mailOptions, function(error, info){
-          if(error){
-              cb(error , null);
-          }
-          else {
-              cb(null , info);
-          }
-
-      });
-  }
-
-	return {
-		sendEmail : sendEmail
-	};
-}*/
