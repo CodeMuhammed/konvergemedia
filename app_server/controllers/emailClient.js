0adfd93035cc66f0d484571d4b06561365daeca5
Muhammed Ali <orion@Muhammeds-MacBook-Pro.local>
2017-05-17 19:41:02 +0100

    updated to sendgrid

--- app_server/controllers/emailClient.js
+++ app_server/controllers/emailClient.js
@@ -1,5 +1,4 @@
 var nodemailer =  require('nodemailer');
-var mg = require('nodemailer-mailgun-transport');
 var sg = require('nodemailer-sendgrid-transport');
 
 // This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
